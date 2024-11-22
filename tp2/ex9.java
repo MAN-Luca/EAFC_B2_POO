dc35bda4e12b9e6c1c758e1b0e756fb8ce8f9833
MAN-Luca <luca.mansutti7@gmail.com>
2024-11-22 11:09:59 +0100

    Ajout boucle EX9

--- tp2/ex9.java
+++ tp2/ex9.java
@@ -1,4 +1,4 @@
-package TravauxDirigés2;
+package tp2;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -10,18 +10,21 @@ public class ex9 {
 		Random random = new Random();
 		
 		int target = random.nextInt(100) + 1;
-		System.out.println("Veuillez entrer un nombre entre 1 et 100\n>>> ");
-		int essai_utilisateur = input.nextInt();
+		int essai_utilisateur = -1;
 		
-		
-		if (essai_utilisateur == target) {
-			System.out.println("Bravo, vous avez trouvé le nombre !");
-		} else if (essai_utilisateur < target) {
-			System.out.println("Plus petit");
-		} else {
-			System.out.println("Plus haut");
-		}
-		
+		do {
+			System.out.println("Veuillez entrer un nombre entre 1 et 100\n>>> ");
+			essai_utilisateur = input.nextInt();
+			if (essai_utilisateur == target) {
+				System.out.println("Bravo, vous avez trouvé le nombre !");
+			} else if (essai_utilisateur < target) {
+				System.out.println("Plus haut");
+			} else {
+				System.out.println("Plus petit");
+			}
+			
+		} while (essai_utilisateur != target);
+			
 		input.close();
 	}
 
